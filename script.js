dda990998fe5b626917e1037df90699f5c5353bd
Sivaranjani-A <112957992+Sivaranjani-A@users.noreply.github.com>
2022-11-03 08:36:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ container.innerHTML =`<div class="container">
     <div class="col-md-12">    
         <img src="https://upload.wikimedia.org/wikipedia/commons/5/59/%E0%AE%A4%E0%AE%BF%E0%AE%B0%E0%AF%81%E0%AE%B5%E0%AE%B3%E0%AF%8D%E0%AE%B3%E0%AF%81%E0%AE%B5%E0%AE%B0%E0%AF%8D_%E0%AE%95%E0%AE%B2%E0%AF%88%E0%AE%AA%E0%AF%8D_%E0%AE%AA%E0%AE%9F%E0%AF%88%E0%AE%AA%E0%AF%8D%E0%AE%AA%E0%AF%81.jpg" alt="Thiruvalluvar image">
         <h2 id="text">குறள்</h2>
+        <h4 id="exp"></h4>
     </div>
 </div>    
 </div>
@@ -26,10 +27,12 @@ async function kural(){
                 
                 document.getElementById("title").innerHTML=`${data1.chap_tam}`;
                 document.getElementById("text").innerHTML=`${data1.line1} <br> ${data1.line2}`;
+                document.getElementById("exp").innerHTML=`<b>விளக்கம்:</b> ${data1.tam_exp}.`;
 
         }else{
                 document.getElementById("text").innerHTML="நன்றி!";
-                document.getElementById("title").innerHTML="திருக்குறள்";  
+                document.getElementById("title").innerHTML="திருக்குறள்";
+                document.getElementById("exp").innerHTML=""; 
         }
 }
 
